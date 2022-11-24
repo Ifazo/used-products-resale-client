b07d54d5ba73ed11b3b02161a2998ff68de047e0
ifaz <ziaulkarimifaz@gmail.com>
2022-11-24 17:04:02 +0600

    navbar added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            Home page
-        </div>
-    );
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
 };
 
 export default Home;
